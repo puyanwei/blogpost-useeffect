ddef9c5bc9f03b3d1224fd6c758f208ea29e20e3
Puyan Wei <puyanwei@gmail.com>
2019-06-01 11:12:37 -0400

    Setup and created counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            number: 0
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ number: this.state.number + 1 });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <p>Number:{this.state.number}</p>
+                <button onClick={() => this.handleClick()}>Adds One</button>
+            </div>
+        );
+    }
 }
-
 export default App;
